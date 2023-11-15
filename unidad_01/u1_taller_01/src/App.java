cd22e091112a3c6825fd4d68e74449a98cac3266
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-11-15 18:00:28 -0500

    Uso de clases

--- unidad_01/u1_taller_01/src/App.java
+++ unidad_01/u1_taller_01/src/App.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<Integer> lista1 = new ArrayList<Integer>();
+        lista1.add(10);
+        lista1.add(100);
+        lista1.add(60);
+        lista1.add(99);
+
+        for (Integer elemento : lista1) {
+            System.out.println(elemento);
+        }
+
+        ArrayList<String> lista2 = new ArrayList<String>();
+        lista2.add("10");
+        lista2.add("100");
+        lista2.add("60");
+        lista2.add("99");
+
+        for (String elemento : lista2) {
+            System.out.println(elemento);
+        }
     }
 }
