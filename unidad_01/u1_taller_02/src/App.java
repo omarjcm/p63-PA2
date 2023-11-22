7932a2e9b017cde3486163da2611240e29293d82
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-11-22 17:58:28 -0500

    Comparacion de objetos.

---
+++ unidad_01/u1_taller_02/src/App.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ArrayList<Object> lista1 = new ArrayList<Object>();
+        lista1.add(10);
+        lista1.add(20);
+        lista1.add(30);
+        lista1.add(40);
+
+        for (Object obj : lista1) {
+            if (obj.equals(40)) {
+                System.out.println(obj);
+            }
+        }
+
+        ArrayList<Object> lista2 = new ArrayList<Object>();
+        lista2.add( new Persona("0937465839", "Juan", "Alba") );
+        lista2.add( new Persona("0937465812", "Maria", "Hinojosa") );
+        lista2.add( new Persona("0937461239", "Andrea", "Calle") );
+        lista2.add( new Persona("0937125839", "Dario", "Betancurt") );
+
+        
+        for (Object obj2 : lista2) {
+            Persona obj3 = new Persona("0937125839", "Dario", "Betancurt");
+    
+            if (((Persona)obj2).equals(obj3)) {
+                System.out.println(obj2);
+            }
+        }
+    }
+}
