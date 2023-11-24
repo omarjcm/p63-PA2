ef0b010c7a36f7b068532eb0ad94a9184db653cc
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-11-24 18:45:12 -0500

    Expresiones Regulares

--- unidad_02/u2_taller_01/src/App4.java
+++ unidad_02/u2_taller_01/src/App4.java
@@ -1,11 +1,11 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class App3 {
+public class App4 {
     public static void main(String[] args) {
-        String texto = "30-10-00009";
+        String texto = "30-10-1980";
 
-        Pattern pattern = Pattern.compile("^([0][1-9]|[1-2][0-9]|[3][0-1])-([0][1-9]|[1][0-2])-\\d{4}$");
+        Pattern pattern = Pattern.compile("^([0][1-9]|[1-2][0-9]|[3][0-1])-([0][1-9]|[1][0-2])-(19|20)[0-9][0-9]$");
         Matcher match = pattern.matcher(texto);
         System.out.println(match.find());        
     }
